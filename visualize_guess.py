6d8618c863b59264ccdbd1f5700231e37e3e8a0b
Daniel Theunissen <theundaniel@gmail.com>
2024-12-13 22:04:39 -0500

    fix coords not lining up in visualization

--- visualize_guess.py
+++ visualize_guess.py
@@ -14,7 +14,7 @@ import numpy as np
 random.seed(546)
 
 # Load the region-to-coordinates mapping
-region_coords = pd.read_csv("clustered_coordinates.csv")
+region_coords = pd.read_csv("clustered_coordinates_fix.csv")
 
 
 def preprocess_image(image):
@@ -199,7 +199,7 @@ def visualize_prediction_two_models(
     return m
 
 
-def predict_and_visualize_on_single_map(model, device, random_images):
+def predict_and_visualize_on_single_map(model1, model2, device, random_images):
     """
     Predict geolocations for multiple images and visualize all predictions on a single map.
     Args:
@@ -237,15 +237,17 @@ def predict_and_visualize_on_single_map(model, device, random_images):
 
         # Actual coordinates for comparison
         actual_coords = (
-            region_coords.loc[val_img_num - 1, "Latitude"],
-            region_coords.loc[val_img_num - 1, "Longitude"],
+            region_coords.iat[val_img_num, 0],
+            region_coords.iat[val_img_num, 1],
         )
 
         # Predict region index using the classification model
-        predicted_region_index = predict_region(model, input_image, device)
+        predicted_region_index1 = predict_region(model1, input_image, device)
+        predicted_region_index2 = predict_region(model2, input_image, device)
 
         # Get the predicted coordinates by mapping the region index
-        predicted_coords = get_coordinates_from_region(predicted_region_index)
+        predicted_coords1 = get_coordinates_from_region(predicted_region_index1)
+        predicted_coords2 = get_coordinates_from_region(predicted_region_index2)
 
         # Initialize the map if it's the first iteration
         if m is None:
@@ -259,16 +261,29 @@ def predict_and_visualize_on_single_map(model, device, random_images):
         popup = folium.Popup(encoded, max_width=2650)
         folium.Marker(location=actual_coords, popup=popup).add_to(m)
 
-        # Add predicted location marker
+        # Add predicted location markers
         folium.Marker(
-            location=predicted_coords,
-            popup=f"Predicted Location (Image {img_num})",
+            location=predicted_coords1,
+            popup="Predicted Location (Model 1)",
             icon=folium.Icon(color="red", icon="info-sign"),
         ).add_to(m)
 
-        # Add a line connecting the two points
+        folium.Marker(
+            location=predicted_coords2,
+            popup="Predicted Location (Model 2)",
+            icon=folium.Icon(color="blue", icon="info-sign"),
+        ).add_to(m)
+
+        # Add lines connecting actual to predictions
         folium.PolyLine(
-            locations=[actual_coords, predicted_coords],
+            locations=[actual_coords, predicted_coords1],
+            color="red",
+            weight=2.5,
+            opacity=0.7,
+        ).add_to(m)
+
+        folium.PolyLine(
+            locations=[actual_coords, predicted_coords2],
             color="blue",
             weight=2.5,
             opacity=0.7,
@@ -278,7 +293,8 @@ def predict_and_visualize_on_single_map(model, device, random_images):
             {
                 "image_number": img_num,
                 "actual_coords": actual_coords,
-                "predicted_coords": predicted_coords,
+                "predicted_coords 1": predicted_coords1,
+                "predicted_coords 2": predicted_coords2,
             }
         )
 
@@ -307,45 +323,45 @@ model2.load_state_dict(
 )  # Load trained model weights
 model2.to(device)
 
-# 14, 47, 51, 65, 79, 10026, 9328
-img_num = 9328
-image_path = f"images/street_view_{img_num}.jpg"
-input_image = Image.open(image_path).convert("RGB")
+# # 14, 47, 51, 65, 79, 10026, 9238
+# img_num = 9238
+# image_path = f"images/street_view_{img_num}.jpg"
+# input_image = Image.open(image_path).convert("RGB")
 
-# Actual coordinates for comparison
-actual_coords = (
-    region_coords.loc[img_num - 1, "Latitude"],
-    region_coords.loc[img_num - 1, "Longitude"],
-)
+# # Actual coordinates for comparison
+# actual_coords = (
+#     region_coords.loc[img_num - 1, "Latitude"],
+#     region_coords.loc[img_num - 1, "Longitude"],
+# )
 
-# Predict region index using the classification model
-predicted_region_index1 = predict_region(model1, input_image, device)
-predicted_region_index2 = predict_region(model2, input_image, device)
+# # Predict region index using the classification model
+# predicted_region_index1 = predict_region(model1, input_image, device)
+# predicted_region_index2 = predict_region(model2, input_image, device)
 
-# Get the predicted coordinates by mapping the region index
-predicted_coords1 = get_coordinates_from_region(predicted_region_index1)
-predicted_coords2 = get_coordinates_from_region(predicted_region_index2)
+# # Get the predicted coordinates by mapping the region index
+# predicted_coords1 = get_coordinates_from_region(predicted_region_index1)
+# predicted_coords2 = get_coordinates_from_region(predicted_region_index2)
 
-# Print predicted coordinates
-print(f"Predicted Coordinates (Model 1): {predicted_coords1}")
-print(f"Predicted Coordinates (Model 2): {predicted_coords2}")
+# # Print predicted coordinates
+# print(f"Predicted Coordinates (Model 1): {predicted_coords1}")
+# print(f"Predicted Coordinates (Model 2): {predicted_coords2}")
 
-# Visualize predictions on a map
-map_view = visualize_prediction_two_models(
-    input_image, actual_coords, predicted_coords1, predicted_coords2
-)
-map_view.save("geolocation_prediction_two_models.html")  # Save to an HTML file
-
-
-# # Select 15 random images from the dataset
-# num_images = 1500  # Total number of images in the dataset
-# random_images = random.sample(range(1, num_images + 1), 5)
+# # Visualize predictions on a map
+# map_view = visualize_prediction_two_models(
+#     input_image, actual_coords, predicted_coords1, predicted_coords2
+# )
+# map_view.save("geolocation_prediction_two_models.html")  # Save to an HTML file
 
 
-# # Run predictions and visualize on a single map
-# results = predict_and_visualize_on_single_map(model2, device, random_images)
+# Select 15 random images from the dataset
+num_images = 1500  # Total number of images in the dataset
+random_images = random.sample(range(1, num_images + 1), 5)
 
-# # Save results to a CSV file
-# results_df = pd.DataFrame(results)
-# results_df.to_csv("predictions/results.csv", index=False)
-# print("Predictions completed and results saved to predictions/results.csv")
+
+# Run predictions and visualize on a single map
+results = predict_and_visualize_on_single_map(model1, model2, device, random_images)
+
+# Save results to a CSV file
+results_df = pd.DataFrame(results)
+results_df.to_csv("predictions/results.csv", index=False)
+print("Predictions completed and results saved to predictions/results.csv")
